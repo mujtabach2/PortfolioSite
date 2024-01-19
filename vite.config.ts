af75f7eebcb4ea2ff503460aa5a611b1369a38f3
mujtabach2 <114252969+mujtabach2@users.noreply.github.com>
2024-01-19 02:46:35 -0500

    workdi

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: './src',
   // Update with your actual domain
   base: '/PortfolioSite/',
   plugins: [react()],
