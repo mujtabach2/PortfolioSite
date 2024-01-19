82cb02529bfa3e2d4b2f75f285b649bc168d794f
mujtabach2 <114252969+mujtabach2@users.noreply.github.com>
2024-01-19 02:27:35 -0500

    moved index

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  
+  root: './src',
   // Update with your actual domain
   base: '/PortfolioSite/',
   plugins: [react()],
